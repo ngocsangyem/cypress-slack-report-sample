84298f7e8d912f1a76c081fb7539f414262c34d0
sagsagg <sang.n@aspireapp.com>
2023-10-21 22:39:13 +0700

    add mocha report

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,4 +6,15 @@ export default defineConfig({
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8888',
   },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
 })
